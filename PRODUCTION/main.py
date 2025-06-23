11f821df0b6e54733ac377104ce1205f13a860f7
Eshaan Ahuja <ahuja.eshaan@gmail.com>
2025-06-23 11:40:43 -0500

    finished v1

---
+++ PRODUCTION/main.py
@@ -0,0 +1,37 @@
+# You import all the IOs of your board
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+# This is the main instance of your keyboard
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+PINS = [board.D3, board.D4, board.D2, board.D1, board.D7, board.D0]
+
+# Tell kmk we are not using a key matrix
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+# Here you define the buttons corresponding to the pins
+# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
+# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.keymap = [
+    [KC.PSCREEN, KC.Macro(Press(KC.LCMD), Tap(KC.V), Release(KC.LCMD)), KC.Macro(Press(KC.LCMD), Press(KC.LSHIFT), Tap(KC.V), Release(KC.LCMD), Release(KC.LSHIFT)), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)), KC.Macro(Press(KC.LCMD), Tap(KC.X), Release(KC.LCMD)), KC.F11]
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
+
+#I'm planning on adding the potentiometer later for volume control. I also don't want a top 3d printed shell because i think the pcb looks cool and i want to leave it exposed
---
+++ README.md
@@ -0,0 +1,27 @@
+# e'sPad
+
+Screenshot of overall pad
+![1750696413647](image/README/1750696413647.png)
+
+Screenshot of schematic
+![1750696481759](image/README/1750696481759.png)
+
+Screenshot of PCB
+![1750696501634](image/README/1750696501634.png)
+
+Screenshot of Case
+![1750696556627](image/README/1750696556627.png)
+Please note: I only want a case that covers the bottom side of the pcb not the top because i think the pcb looks cool and i don't want to cover it, so i only have a bottom case. I asked in the slack and people said that you've let them do it before so it should be ok
+![1750696652983](image/README/1750696652983.png)
+
+Bill of materials:
+PCB
+Case
+6 switches
+6 keycaps
+1 potentiometer
+XIAO
+
+I have my own 3d printer and can print the case myself if that's easier
+
+Sorry if i missed anything
